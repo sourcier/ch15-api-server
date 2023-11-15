7e8a6c081e99d6589c7891a62fd3a2c3ec5e4d24
Roger Rajaratnam <roger@sourcier.uk>
2023-11-15 20:00:24 +0000

    create api endpoints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./server.js");
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV = "development" } = process.env;
 
 server.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}`);
--- middleware.js
+++ middleware.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 module.exports = function (app) {
   app.use(express.static("public"));
+
+  const cars = [{ name: "Tesla" }, { name: "Ford" }];
+
+  app.get("/api/v1/cars", (req, res) => {
+    res.status(200).json(cars);
+  });
+
+  app.post("/api/v1/cars", (req, res) => {
+    const newCar = req.body;
+    cars.push(newCar);
+    req.sendStatus(201);
+  });
 };
